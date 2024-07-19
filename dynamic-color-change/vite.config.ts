6fb034eb3012f7e03f9e4c531b22c97882b65ab3
hokuto-kato <hokuto15@gmail.com>
2024-07-19 15:23:05 +0900

    feat: ファイル構造を修正し、ビルド設定を更新
    - ファイルパスの再構成と修正
    - `vite.config.ts` および `tsconfig.json` のビルド設定を更新
    - `script.js`, `style.css`, および `index.html` のファイルパスを修正
    - ファイル構造を整理、再構成を行い、ビルド設定を更新

--- dynamic-color-change/vite.config.ts
+++ dynamic-color-change/vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 
+const baseDir = 'dynamic-color-change';
+
 export default defineConfig({
   build: {
-    outDir: '../docs/dynamic-color-change/',
+    outDir: `../docs/${baseDir}/`,
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        entryFileNames: 'script/script.js',
+        entryFileNames: `${baseDir}/script/script.js`,
         assetFileNames: (assetInfo) => {
           if (assetInfo.name && assetInfo.name.endsWith('.css')) {
-            return 'style/style.css'
+            return `${baseDir}/style/style.css`
           }
-          return 'assets/[name]-[hash][extname]'
+          return `assets/${baseDir}/[name]-[hash][extname]`
         }
       }
     }
